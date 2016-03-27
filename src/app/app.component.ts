43bf6e6041f2ceb4b9e43caf01d83daa8b2abc9e
Djuri Baars <dsbaars@gmail.com>
2016-03-27 23:23:29 +0200

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from 'angular2/core';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import { DashboardComponent } from '../dashboard/dashboard.component';
+
+
+@Component({
+  moduleId: __moduleName,
+  selector: 'my-app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css'],
+  directives: [ROUTER_DIRECTIVES],
+  providers: [
+    ROUTER_PROVIDERS,
+  ]
+})
+@RouteConfig([
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  }
+])
+export class AppComponent {
+  public title = 'Application title'
+}
